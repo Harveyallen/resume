e64e22ab708c9d522d37c6b970db4d979e8c427d
Harvey Allen <zoulong2008@126.com>
2017-11-19 11:50:15 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="page">
+    <header>
+      <Topbar/>
+    </header>
+    <main>
+      <ResumeEditor/>
+      <ResumePreview/>
+    </main>
+  </div>
+</template>
+
+<script>
+  import 'normalize.css/normalize.css'
+  import './assets/reset.css'
+  import Topbar from './components/Topbar'
+  import ResumeEditor from './components/ResumeEditor'
+  import ResumePreview from './components/ResumePreview.vue'
+  import icons from './assets/icons'
+  import store from './store/index'
+  import './assets/ui.scss'
+
+export default {
+  name: 'app',
+//  data: function () {  // 👈 注意空格
+//           return {
+//             text: '你好'
+//           }
+//       },
+  store,
+  components:{Topbar,ResumeEditor,ResumePreview},
+  created(){
+    document.body.insertAdjacentHTML('afterbegin', icons) //
+  }
+
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/ResumeEditor.vue
@@ -0,0 +1,118 @@
+ <template>
+  <div id="resumeEditor">
+    <nav>
+      <ol>
+        <li v-for="(item,index) in resume.config"
+            :class="{active: item.field===selected}"
+            @click="selected=item.field">
+            <svg class="icon">
+              <use :xlink:href="`#icon-${item.icon}`"></use>
+            </svg>
+
+        </li>
+      </ol>
+    </nav>
+    <ol class="panels">
+      <li v-for="item in resume.config" v-show="item.field===selected">
+        <div v-if="resume[item.field] instanceof Array">
+          <div class="subitem" v-for="subitem in resume[item.field]">
+            <div class="resumeField" v-for="(value,key) in subitem">
+              <label> {{key}} </label>
+              <input type="text" :value="value">
+            </div>
+            <hr>
+          </div>
+        </div>
+        <div v-else class="resumeField" v-for="(value,key) in resume[item.field]">
+          <label> {{key}} </label>
+          <input type="text" v-model="resume[item.field][key]">
+        </div>
+      </li>
+    </ol>
+  </div>
+  </template>
+
+  <script>
+    export default{
+      name: 'ResumeEditor',
+
+      computed:{
+
+        selected:{
+          get(){
+            return this.$store.state.selected
+          },
+          set(value){
+            return this.$store.commit('switchTab', value)
+          }
+        },
+        resume (){
+             return this.$store.state.resume
+          }
+      },
+
+      methods:{
+
+      }
+    }
+  </script>
+
+  <style lang="scss" scoped>
+    #resumeEditor{
+      min-width: 35%;
+      background: #fff;
+      box-shadow: 0 1px 3px 0 rgba(0,0,0,0.25);
+      display: flex;
+      flex-direction: row;
+      overflow:hidden;
+      >nav{
+         width: 80px;
+         background-color: #000;
+         color: #fff;
+         >ol{
+            >li{
+               height: 48px;
+               display: flex;
+               justify-content: center;
+               align-items: center;
+               margin: 16px 0;
+               &.active{
+                  background-color: #fff;
+                  color: black;
+                }
+             }
+          }
+       }
+    >.panels{
+       flex-grow: 1;
+       >li{
+        padding:24px;
+        }
+     }
+    svg.icon{
+      width: 24px;
+      height: 24px;
+    }
+    }
+    ol{
+      list-style: none;
+    }
+    .resumeField{
+      >label{
+         display: block;
+       }
+       input[type=text]{
+         margin: 16px 0;
+         border: 1px solid #ddd;
+         box-shadow: inset 0 1px 3px 0 rgba(0,0,0,0.25);
+         width: 100%;
+         height: 40px;
+         padding: 0 8px;
+       }
+    }
+    hr{
+      border: none;
+      border-top: 1px solid #ddd;
+      margin: 24px 0;
+    }
+  </style>
